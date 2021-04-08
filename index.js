eeb31c4339f554f2bda60663811ecc509649bc1e
singhjptech <singhjptech@gmail.com>
2021-04-09 00:21:38 +0100

    move function

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
+let currentSnake = [12, 11, 10]
+let direction = 1
 
 function createGrid() {
+    //creating 100 grid squares
     for (let i = 0; i < 100; i++) {
         const square = document.createElement('div')
         square.classList.add('square')
@@ -12,3 +15,16 @@ function createGrid() {
     }  
 }
 createGrid()
+
+currentSnake.forEach(i => squares[i].classList.add('snake'))
+
+function move() {
+    //movement by poping & unshifting snake from squares
+    const tail = currentSnake.pop()
+    squares[tail].classList.remove('snake')
+    currentSnake.unshift(currentSnake[0] + direction)
+    squares[currentSnake[0]].classList.add('snake')
+}
+move()
+
+//let timerId = setInterval(move, 1000)
