bff77759e93448cef2b6717f479bb05d2e9a0e3e
singhjptech <singhjptech@gmail.com>
2021-04-09 01:25:08 +0100

    control function

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
-let currentSnake = [12, 11, 10]
+let currentSnake = [2,1,0]
 let direction = 1
+let width = 10
 
 function createGrid() {
     //creating 100 grid squares
@@ -27,4 +28,22 @@ function move() {
 }
 move()
 
-//let timerId = setInterval(move, 1000)
+let timerId = setInterval(move, 1000)
+
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+    if (e.keyCode === 39) {        
+        direction = 1
+    } else if (e.keyCode === 38) {        
+        direction = -width
+    } else if (e.keyCode === 37) {        
+        direction = -1
+    } else if (e.keyCode === 40) {        
+        direction = +width
+    }
+}
+document.addEventListener('keyup', control)
