f4a0fd390267ada9d813ec02dc19ebcea897a0be
singhjptech <singhjptech@gmail.com>
2021-05-05 00:22:21 +0100

    score added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
 const width = 10
 let appleIndex = 0
+let score = 0
+let intervalTime = 1000
+let speed = 0.9
 
 function createGrid() {
     //creating 100 grid squares
@@ -45,14 +48,21 @@ function move() {
         currentSnake.push(tail)        
         //generate new apple
         generateApples()
-        
+        //add one to the score
+        score++        
+        //display our score
+        scoreDisplay.textContent = score   
+        //speed up our snake
+        clearInterval(timerId)
+        intervalTime = intervalTime * speed
+        timerId = setInterval(move, intervalTime)
     }
 
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 
-let timerId = setInterval(move, 1000)
+let timerId = setInterval(move, intervalTime)
 
 function generateApples() {
     do {
